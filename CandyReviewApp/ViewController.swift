e42ba53b1832ba58f23fa9f4ca88c6d99d2bbd0d
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-10-31 13:07:13 -1000

    Initial Commit

---
+++ CandyReviewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CandyReviewApp
+//
+//  Created by Marijoy Jatico on 10/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
